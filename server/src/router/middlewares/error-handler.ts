0c839b11117ba242cbae9722f0bbfcde18db4f46
martinwepner <martin.wepner@icloud.com>
2023-07-07 15:15:16 +0200

    remove unused error handler middleware

--- server/src/router/middlewares/error-handler.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorRequestHandler } from "express"
-
-export const ErrorHandler = (): ErrorRequestHandler => (err, _req, res, _next) => {
-	if (err) {
-		console.error(err)
-
-		res.status(500).send("Internal server error")
-	}
-}
